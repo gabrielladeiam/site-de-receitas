21addd7ffe1d2424b8ec042e50012f54d9d7b7b3
Pedro Frattezi Silva <pfrattezi@gmail.com>
2024-05-21 18:02:46 -0300

    chore: add todos on template files

---
+++ client/src/components/Recipes/RecipeCard.jsx
@@ -0,0 +1,4 @@
+/* TODO
+- Create a RecipeCard component that will display the recipe's name, image, and a button to view the recipe details
+- This component here should be used in the Recipes component, that will focus on recipe cards demonstration
+*/
