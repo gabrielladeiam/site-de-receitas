03509dedc76ac1a4a1bd40f14b47f84a241c96f2
Pedro Frattezi Silva <pfrattezi@gmail.com>
2024-05-21 18:00:47 -0300

    refactor: update recipes to use new react query hook and lint style rules

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -1,60 +1,33 @@
 import "./recipes.styles.css";
-import { useEffect, useState,  } from "react";
 import { Ingredients } from "../Ingredients/Ingredients";
-import { useRecipesQuery } from "../../hooks/recipes"
+import { useRecipesQuery, useRemoveRecipeMutation } from "../../hooks/recipes"
 import { Link } from "react-router-dom";
 import * as icons from "../../assets";
 
 export function Recipes({ totalPrice, searchQuery }) {
-  
-  // TODO - Remove state in favour of useRecipesQuery
-  const [recipeList, setRecipeList] = useState();
-  const { data , isLoading}  = useRecipesQuery(searchQuery)
+  const removeRecipeMutation = useRemoveRecipeMutation();
+  const { data: recipeList , isLoading}  = useRecipesQuery(searchQuery);
 
-  console.log({ data })
-  useEffect(() => {
-    setRecipeList(data)
-  }, [data])
-
-  //remove uma receita
-  const removeRecipe = async (recipe) => {
-    if (
-      window.confirm(`Você realmente quer deletar a receita ${recipe.name}?`)
-    ) {
-      try {
-        const response = await fetch(
-          `http://localhost:3001/recipes/${recipe.id}`,
-          {
-            method: "DELETE",
-          }
-        );
-        if (!response.ok) {
-          throw new Error("Falha ao excluir a receita");
-        }
-        console.log("Receita excluída com sucesso!");
-      } catch (error) {
-        console.error("Falha ao excluir a receita: ", error.message);
-      }
-    }
-  };
-
-  // TODO - Extract condition
-  if (!recipeList || recipeList.length === 0 || isLoading) {
-    return null; // Não há receitas, nada será renderizado
+  const shouldRender = !recipeList || recipeList.length === 0 || isLoading;
+  if (shouldRender) {
+    return null;
   }
 
   // TODO- Refactor styles, extract internal component
   return (
     <>
       {recipeList.map((recipe, index) => (
+        // Extract this into a Recipe component
         <div key={index} id={recipe.id} className="recipe">
           <button
             className="delete"
-            onClick={() => removeRecipe(recipe)}
+            onClick={() => removeRecipeMutation(recipe)}
           ></button>
+
           <Link to={`/recipes/${recipe.id}/edit`} state={recipe}>
             <button className="edit"></button>
           </Link>
+
           <div className="ingredients-tittles">
             <img src={recipe.icon} alt={recipe.name} />
 
@@ -62,8 +35,10 @@ export function Recipes({ totalPrice, searchQuery }) {
             <h2>Ingredients</h2>
             <h3>{totalPrice}</h3>
           </div>
+
           <div className="ingredients">
             {/* Not use index as list key, use descriptive names */}
+            {/* Extract this into a ingredient list component */}
             {recipe.ingredients.map((ingredient, i) => (
               <Ingredients
                 key={i}
